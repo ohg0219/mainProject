edb6bdb8f9d6432e14c66cd742da07ca1410452d
DESKTOP-28T0QDL\dpwls <dpwls2136@naver.com>
2021-03-14 12:53:17 +0900

    웰컴페이지 임시

--- src/main/java/com/thisisthat/user/register/controller/UserRegisterController.java
+++ src/main/java/com/thisisthat/user/register/controller/UserRegisterController.java
@@ -3,6 +3,7 @@ package com.thisisthat.user.register.controller;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -44,7 +45,7 @@ public class UserRegisterController {
 	 * @return
 	 */
 	@PostMapping("/register.do")
-	public String insertUser(UserRegisterVO vo,
+	public String insertUser(UserRegisterVO vo, Model model,
 			@RequestParam("phone1")String phone1,
 			@RequestParam("phone2")String phone2,
 			@RequestParam("phone3")String phone3,
@@ -53,6 +54,7 @@ public class UserRegisterController {
 		vo.setPhone(phone1+phone2+phone3);
 		vo.setPassword(BCrypt.hashpw(password, BCrypt.gensalt()));
 		userRegisterService.insertUser(vo);
+		model.addAttribute("user", vo);
 		return "/user/joinResult";
 	}
 	
